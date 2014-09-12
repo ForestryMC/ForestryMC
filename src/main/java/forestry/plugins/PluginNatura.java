d6c3c8093c8ba4b452740e99d30a6589b733ea6c
mezz <tehgeek@gmail.com>
2014-09-12 02:01:03 -0700

    Revert "Updated PluginNatura for latest Natura"
    (it's not released yet)
    
    This reverts commit fe86b4d08bb8e08c55f8c2ee61e08a6226065771.

--- src/main/java/forestry/plugins/PluginNatura.java
+++ src/main/java/forestry/plugins/PluginNatura.java
@@ -50,14 +50,14 @@ public class PluginNatura extends NativePlugin {
 		logWillow = GameRegistry.findBlock(NATURA, "willow");
 		leavesNatura = GameRegistry.findBlock(NATURA, "floraleaves");
 		saplingNatura = GameRegistry.findBlock(NATURA, "florasapling");
-		saplingNaturaRare = GameRegistry.findBlock(NATURA, "RareSapling");
+		saplingNaturaRare = GameRegistry.findBlock(NATURA, "Rare Sapling");
 
 		ArrayList<String> saplingItemKeys = new ArrayList<String>();
 
 		if(saplingNatura != null)
 			saplingItemKeys.add("florasapling");
 		if(saplingNaturaRare != null)
-			saplingItemKeys.add("RareSapling");
+			saplingItemKeys.add("Rare Sapling");
 
 		for (String key : saplingItemKeys) {
 			Item saplingItem = GameRegistry.findItem(NATURA, key);
