bfd49b107dc07b87a8979213be076ef77df37649
mezz <tehgeek@gmail.com>
2015-09-23 03:08:45 -0700

    Remove PropolisPipe (Moved to BC Compat) and update BC API version

--- src/main/java/forestry/plugins/PluginManager.java
+++ src/main/java/forestry/plugins/PluginManager.java
@@ -83,7 +83,6 @@ public class PluginManager {
 		BUILDCRAFT_RECIPES(new PluginBuildCraftRecipes()),
 		BUILDCRAFT_STATEMENTS(new PluginBuildCraftStatements()),
 		BUILDCRAFT_TRANSPORT(new PluginBuildCraftTransport()),
-		PROPOLIS_PIPE(new PluginPropolisPipe()),
 		
 		AGRICRAFT(new PluginAgriCraft()),
 		BIOMESOPLENTY(new PluginBiomesOPlenty()),
