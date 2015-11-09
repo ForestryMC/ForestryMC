8465ff358ae6a52c77422eb5b2be676e030fd8d1
mezz <tehgeek@gmail.com>
2015-11-08 20:23:51 -0800

    Update Lang, organize error codes

--- src/main/java/forestry/core/errors/EnumErrorCode.java
+++ src/main/java/forestry/core/errors/EnumErrorCode.java
@@ -17,47 +17,68 @@ import forestry.core.config.Constants;
 
 public enum EnumErrorCode implements IErrorState {
 
-	UNKNOWN("unknown"),
-	INVALIDBIOME("invalidBiome"),
-	ISRAINING("isRaining"),
-	NOTRAINING("notRaining"),
-	NOFUEL("noFuel"), // Biogas & Peat-fired
-	NOHEAT("noHeat"), // Biogas engine
-	NODISPOSAL("noDisposal"),
-	NORESOURCE("noResource"),
-	NOTGLOOMY("notGloomy"),
-	NOTLUCID("notLucid"),
-	NOTDAY("notDay"),
-	NOTNIGHT("notNight"),
-	NOFLOWER("noFlower"),
-	NOQUEEN("noQueen"),
-	NODRONE("noDrone"),
-	NOSKY("noSky"),
-	NOSPACE("noSpace"),
-	NOSPACETANK("noSpaceTank", "noLiquid"),
-	NORECIPE("noRecipe"),
-	NOENERGYNET("noEnergyNet"),
-	NOTHINGANALYZE("noSpecimen"),
-	FORCEDCOOLDOWN("forcedCooldown"),
-	NOHONEY("noHoney"),
-	NOTPOSTPAID("notPostpaid", "noStamps"),
-	NORECIPIENT("noRecipient"),
-	NOTALPHANUMERIC("notAlphaNumeric"),
-	NOTUNIQUE("notUnique"),
-	NOSTAMPS("noStamps"),
-	NOCIRCUITBOARD("noCircuitBoard"),
-	NOCIRCUITLAYOUT("noCircuitLayout"),
-	WRONGSTACKSIZE("wrongStacksize"),
+	@Deprecated
+	UNKNOWN("unknown"), // Congratulations, you found a glitch.
+	@Deprecated
+	WRONGSTACKSIZE("wrongStacksize"), // Only single items can be used here.
+	@Deprecated
+	NODISPOSAL("noDisposal"), // Insufficient space to dispose of waste products.
+
+	// Bees
+	INVALIDBIOME("invalidBiome"), // Machine or inhabitants cannot work in this biome.
+	ISRAINING("isRaining"), // Operation is only possible when it is not raining.
+	NOTRAINING("notRaining"), // Operation is only possible when it is raining.
+	NOTGLOOMY("notGloomy"), // A lower light level is required for operation.
+	NOTLUCID("notLucid"), // A higher light level is required for operation
+	NOTDAY("notDay"), // Operation is only possible during the day.
+	NOTNIGHT("notNight"), // Operation is only possible during the night.
+	NOFLOWER("noFlower"), // Hive members are not finding the right flowers.
+	NOQUEEN("noQueen"), // Supply this hive with a queen or a princess and a drone.
+	NODRONE("noDrone"), // Mating requires a drone present.
+	NOSKY("noSky"), // Clear the area above the machine.
+	
+	// Machines
+	NORESOURCE("noResource"), // More resources need to be supplied for operation.
+	NORECIPE("noRecipe"), // No matching recipe was found for the supplied resources.
+	NOSPACE("noSpace"), // Empty this machine's inventory.
+	NOSPACETANK("noSpaceTank", "noLiquid"), // Empty this machine's liquid tank.
+
+	// Alyzer
+	NOHONEY("noHoney"), // This gadget requires honey drops or honeydew for operation.
+	NOTHINGANALYZE("noSpecimen"), // Supply specimen to analyze.
+
+	// Engines
+	FORCEDCOOLDOWN("forcedCooldown"), // Engine has overheated and is forced into cooldown.
+	NOFUEL("noFuel"), // (Biogas & Peat-fired) Replenish this machine's fuel supplies.
+	NOHEAT("noHeat"), // (Biogas engine) Refill the heating tank.
+	NOENERGYNET("noEnergyNet"), // Your world is barren of any electricity. (Install IndustrialCraft\u00b2.)
+
+	// Trade Station
+	NOSTAMPS("noStamps"), // The trade station requires more stamps to pay postage.
+	NOPAPER("noPaper"), // The trade station requires more paper to send letters.
+	NOSUPPLIES("noSupplies", "noResource"), // The trade station requires more supplies to send.
+	NOTRADE("noTrade", "noResource"), // The trade station requires items to Send and Receive.
+
+	// Trade Station naming
+	NOTALPHANUMERIC("notAlphaNumeric"), // Given string must consist of letters and numbers only.
+	NOTUNIQUE("notUnique"), // Given string must be unique in this world. Choose a different one.
+
+	// Letters
+	NOTPOSTPAID("notPostpaid", "noStamps"), // Apply more stamps to pay the postal service.
+	NORECIPIENT("noRecipient"), // You need to address your letter to a recipient to send it.
+
+	// Circuit Boards
+	NOCIRCUITBOARD("noCircuitBoard"), // Insert a circuit board to solder the selected tubes onto it.
+	NOCIRCUITLAYOUT("noCircuitLayout"), // No layouts available due to the current game settings.
+	CIRCUITMISMATCH("circuitMismatch"), // Amount of tubes does not match size of circuit board.
+
 	NOFERTILIZER("noFertilizer"),
 	NOFARMLAND("noFarmland"),
-	CIRCUITMISMATCH("circuitMismatch"),
+
 	NOLIQUID("noLiquid"),
-	NOPAPER("noPaper"),
-	NOSUPPLIES("noSupplies", "noResource"),
-	NOTRADE("noTrade", "noResource"),
-	NOPOWER("noPower"),
-	NOREDSTONE("noRedstone", "disabled"), // needs redstone signal
-	DISABLED("disabledRedstone", "disabled") // disabled by redstone signal
+	NOPOWER("noPower"), // This machine requires RF power to do work.
+	NOREDSTONE("noRedstone", "disabled"), // This machine requires a redstone signal to activate it.
+	DISABLED("disabledRedstone", "disabled") // This machine is being disabled by a redstone signal.
 	;
 
 	private final String name;
