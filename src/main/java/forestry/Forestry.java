30286fd0ad365760c4ddde9465aa9afc0bd8a63a
mezz <tehgeek@gmail.com>
2016-05-29 01:42:24 -0700

    Update to latest Forge

--- src/main/java/forestry/Forestry.java
+++ src/main/java/forestry/Forestry.java
@@ -49,7 +49,7 @@ import forestry.plugins.PluginManager;
 		name = Constants.MOD_ID,
 		version = Constants.VERSION,
 		guiFactory = "forestry.core.config.ForestryGuiConfigFactory",
-		dependencies = "required-after:Forge@[12.17.0.1922,);"
+		dependencies = "required-after:Forge@[12.17.0.1925,);"
 				+ "after:Buildcraft|Core@[7.2,);"
 				+ "after:BuildCraft|Energy@[7.2,);"
 				+ "after:ExtrabiomesXL;"
