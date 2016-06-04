856322b208a3ad36f450bb0f17a6822328c50a4c
mezz <tehgeek@gmail.com>
2016-06-03 17:29:54 -0700

    Update Forge to fix the crash with cactus placement and BoP grass

--- src/main/java/forestry/Forestry.java
+++ src/main/java/forestry/Forestry.java
@@ -49,7 +49,7 @@ import forestry.plugins.PluginManager;
 		name = "Forestry",
 		version = Constants.VERSION,
 		guiFactory = "forestry.core.config.ForestryGuiConfigFactory",
-		dependencies = "required-after:Forge@[12.17.0.1932,);"
+		dependencies = "required-after:Forge@[12.17.0.1941,);"
 				+ "after:Buildcraft|Core@[7.2,);"
 				+ "after:BuildCraft|Energy@[7.2,);"
 				+ "after:ExtrabiomesXL;"
