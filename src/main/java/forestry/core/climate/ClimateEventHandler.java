8639148f20328293fd587547af477b34396d3558
Nedelosk <nedelosk@gmail.com>
2016-06-13 22:56:29 +0200

    Fix #1318 1.9.4 Crash with .153 version on world load

--- src/main/java/forestry/core/climate/ClimateEventHandler.java
+++ src/main/java/forestry/core/climate/ClimateEventHandler.java
@@ -30,8 +30,10 @@ public class ClimateEventHandler {
 					region.updateClimate();
 				}
 			}
-			for(IClimateSource source : ForestryAPI.climateManager.getSources().get(Integer.valueOf(dim)).values()){
-				source.changeClimate(ticks, ForestryAPI.climateManager.getRegionForPos(event.world, source.getPos()));
+			if(ForestryAPI.climateManager.getSources().get(Integer.valueOf(dim)) != null){
+				for(IClimateSource source : ForestryAPI.climateManager.getSources().get(Integer.valueOf(dim)).values()){
+					source.changeClimate(ticks, ForestryAPI.climateManager.getRegionForPos(event.world, source.getPos()));
+				}
 			}
 			serverTicks.put(Integer.valueOf(dim), ticks+1);
 		}
