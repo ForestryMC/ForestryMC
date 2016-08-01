cd9f82001c885db266904d64f18902a186c8905a
mezz <tehgeek@gmail.com>
2016-07-31 17:10:59 -0700

    Fix #1366 Alveary in the Nether does not have Hellish temperature

--- src/main/java/forestry/apiculture/multiblock/AlvearyController.java
+++ src/main/java/forestry/apiculture/multiblock/AlvearyController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import forestry.api.core.BiomeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -359,8 +360,11 @@ public class AlvearyController extends RectangularMultiblockControllerBase imple
 	@Override
 	public EnumTemperature getTemperature() {
 		IBeeModifier beeModifier = BeeManager.beeRoot.createBeeHousingModifier(this);
-		if (beeModifier.isHellish() && tempChange >= 0) {
-			return EnumTemperature.HELLISH;
+		BiomeGenBase biome = getBiome();
+		if (beeModifier.isHellish() || BiomeHelper.isBiomeHellish(biome)) {
+			if (tempChange >= 0) {
+				return EnumTemperature.HELLISH;
+			}
 		}
 
 		return EnumTemperature.getFromValue(getExactTemperature());
