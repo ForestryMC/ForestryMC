a3a1f7720ecf54c908b45d75ca987b357342667d
mezz <tehgeek@gmail.com>
2016-08-03 02:21:27 -0700

    Fix #1371 IC2 Rubber farm does not harvest rubber properly

--- src/main/java/forestry/farming/logic/CropRubber.java
+++ src/main/java/forestry/farming/logic/CropRubber.java
@@ -10,19 +10,24 @@
  ******************************************************************************/
 package forestry.farming.logic;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableCollection;
+import forestry.core.config.Constants;
+import forestry.core.network.packets.PacketFXSignal;
+import forestry.core.proxy.Proxies;
+import forestry.core.utils.ItemStackUtil;
+import forestry.plugins.compat.PluginIC2;
 import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import forestry.core.config.Constants;
-import forestry.core.network.packets.PacketFXSignal;
-import forestry.core.proxy.Proxies;
-import forestry.plugins.compat.PluginIC2;
 
 public class CropRubber extends CropDestroy {
 
@@ -34,10 +39,44 @@ public class CropRubber extends CropDestroy {
 	 * Convert a "wet" rubber log blockstate into the dry version.
 	 * Total hack since we don't have access to the blockstates.
 	 */
-	private static IBlockState getReplantState(IBlockState sappyState) {
-		Block block = sappyState.getBlock();
-		int sappyMeta = block.getMetaFromState(sappyState);
-		return block.getStateFromMeta(sappyMeta - 4);
+	private static <T extends Comparable<T>> IBlockState getReplantState(IBlockState sappyState) {
+		if (hasRubberToHarvest(sappyState)) {
+			for (Map.Entry<IProperty<?>, Comparable<?>> wetPropertyEntry : sappyState.getProperties().entrySet()) {
+				String valueWetString = wetPropertyEntry.getValue().toString();
+				String valueDryString = valueWetString.replace("wet", "dry");
+				IProperty<?> property = wetPropertyEntry.getKey();
+
+				IBlockState baseState = sappyState.getBlock().getBlockState().getBaseState();
+				IBlockState dryState = getStateWithValue(baseState, property, valueDryString);
+				if (dryState != null) {
+					return dryState;
+				}
+			}
+		}
+
+		return sappyState.getBlock().getDefaultState();
+	}
+
+	public static boolean hasRubberToHarvest(IBlockState blockState) {
+		Block block = blockState.getBlock();
+		if (ItemStackUtil.equals(block, PluginIC2.rubberWood)) {
+			ImmutableCollection<Comparable<?>> propertyValues = blockState.getProperties().values();
+			for (Comparable<?> propertyValue : propertyValues) {
+				if (propertyValue.toString().contains("wet")) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Nullable
+	private static <T extends Comparable<T>> IBlockState getStateWithValue(IBlockState baseState, IProperty<T> property, String valueString) {
+		Optional<T> value = property.parseValue(valueString);
+		if (value.isPresent()) {
+			return baseState.withProperty(property, value.get());
+		}
+		return null;
 	}
 
 	@Override
