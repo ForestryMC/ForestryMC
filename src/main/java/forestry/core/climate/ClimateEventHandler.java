7da56f93f2934efee58b67dd6623f08e48bec044
Aroma1997 <aroma1997@aroma1997.org>
2016-09-05 13:15:41 +0200

    Fix #1414

--- src/main/java/forestry/core/climate/ClimateEventHandler.java
+++ src/main/java/forestry/core/climate/ClimateEventHandler.java
@@ -1,27 +1,26 @@
 package forestry.core.climate;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import forestry.api.core.ForestryAPI;
-import forestry.api.core.climate.IClimateManager;
 import forestry.api.core.climate.IClimateRegion;
 import forestry.api.core.climate.IClimateSource;
+
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.event.world.WorldEvent;
+
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class ClimateEventHandler {
 
 	Map<Integer, Integer> serverTicks;
-	
+
 	public ClimateEventHandler() {
 		serverTicks = new HashMap<>();
 	}
-	
+
 	@SubscribeEvent
 	public void onWorldTick(TickEvent.WorldTickEvent event) {
 		Integer dim = Integer.valueOf(event.world.provider.getDimension());
@@ -41,7 +40,7 @@ public class ClimateEventHandler {
 			Map<Integer, Map<BlockPos, IClimateSource>> sources = ForestryAPI.climateManager.getSources();
 			if(sources != null && sources.containsKey(dim)){
 				for(IClimateSource source : sources.get(dim).values()){
-					IClimateRegion region = ForestryAPI.climateManager.getRegionForPos(event.world, source.getPos());
+					IClimateRegion region = ForestryAPI.climateManager.getRegionForPos(event.world, source.getCoordinates());
 					if(region != null){
 						if(ticks % source.getTicksForChange(region) == 0){
 							source.changeClimate(ticks, region);
@@ -52,5 +51,5 @@ public class ClimateEventHandler {
 			serverTicks.put(dim, ticks+1);
 		}
 	}
-	
+
 }
