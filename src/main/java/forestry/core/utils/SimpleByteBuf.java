bd1b93b314bcafccd9d94e0faae017f8935ca528
Nedelosk <nedelosk@gmail.com>
2017-11-24 00:33:33 +0100

    Remove Chromosome Type Check

--- src/main/java/forestry/core/utils/SimpleByteBuf.java
+++ src/main/java/forestry/core/utils/SimpleByteBuf.java
@@ -81,27 +81,6 @@ public class SimpleByteBuf {
 	}
 
 	/**
-	 * Every bit of this integer represents a chromosome type of chromosome array.
-	 * If the bit is one, the array contains a chromosome at the position of the chromosome type.
-	 * If the bit is zero, the array not contains a chromosome at the position of the chromosome type.
-	 *
-	 * The method {@link #readChromosomes(ISpeciesRoot)} uses this integer do check if a byte array contains the data
-	 * of a specific chromosome.
-	 */
-	private int getValidChromosomes(IChromosome[] chromosomes, IChromosomeType[] chromosomeTypes){
-		int types = 0;
-		for (IChromosomeType type : chromosomeTypes) {
-			int index = type.ordinal();
-			if (index >= chromosomes.length || chromosomes[index] == null) {
-				types <<= 1;
-				continue;
-			}
-			types = (types << 1) + 1;
-		}
-		return types;
-	}
-
-	/**
 	 * Writes the chromosomes to the internal byte array.
 	 *
 	 * @param chromosomes The chromosomes that should be written.
@@ -109,7 +88,6 @@ public class SimpleByteBuf {
 	 */
 	public void writeChromosomes(IChromosome[] chromosomes, ISpeciesRoot speciesRoot){
 		IChromosomeType[] chromosomeTypes = speciesRoot.getKaryotype();
-		writeVarInt(getValidChromosomes(chromosomes, chromosomeTypes));
 		for (IChromosomeType type : chromosomeTypes) {
 			int index = type.ordinal();
 			if (index >= chromosomes.length) {
@@ -130,7 +108,6 @@ public class SimpleByteBuf {
 	 */
 	public IChromosome[] readChromosomes(ISpeciesRoot speciesRoot){
 		IChromosomeType[] chromosomeTypes = speciesRoot.getKaryotype();
-		int types = readVarInt();
 		IChromosome[] chromosomes = new IChromosome[chromosomeTypes.length];
 
 		String primarySpeciesUid = null;
@@ -138,10 +115,8 @@ public class SimpleByteBuf {
 
 		for (IChromosomeType type : chromosomeTypes) {
 			int index = type.ordinal();
-			int bit = (1 << chromosomeTypes.length - index - 1);
-			boolean isValid = (bit & types) == bit;
 
-			Chromosome chromosome = readChromosome(type, isValid, primarySpeciesUid, secondarySpeciesUid);
+			Chromosome chromosome = readChromosome(type, primarySpeciesUid, secondarySpeciesUid);
 			chromosomes[index] = chromosome;
 
 			if (type == speciesRoot.getSpeciesChromosomeType()) {
@@ -152,19 +127,13 @@ public class SimpleByteBuf {
 		return chromosomes;
 	}
 
-	private Chromosome readChromosome(IChromosomeType type, boolean isValid, ChromosomeInfo info){
-		return readChromosome(type, isValid, info.activeSpeciesUid, info.inactiveSpeciesUid);
+	private Chromosome readChromosome(IChromosomeType type, ChromosomeInfo info){
+		return readChromosome(type, info.activeSpeciesUid, info.inactiveSpeciesUid);
 	}
 
-	private Chromosome readChromosome(IChromosomeType type, boolean isValid, @Nullable String activeSpeciesUid, @Nullable String inactiveSpeciesUid){
-		IAllele firstAllele = null;
-		IAllele secondAllele = null;
-
-		if (isValid) {
-			firstAllele = readAllele();
-			secondAllele = readAllele();
-		}
-
+	private Chromosome readChromosome(IChromosomeType type, @Nullable String activeSpeciesUid, @Nullable String inactiveSpeciesUid){
+		IAllele firstAllele = readAllele();
+		IAllele secondAllele = readAllele();
 		return Chromosome.create(activeSpeciesUid, inactiveSpeciesUid, type, firstAllele, secondAllele);
 	}
 
@@ -174,24 +143,16 @@ public class SimpleByteBuf {
 	public ChromosomeInfo readChromosome(IChromosomeType chromosomeType){
 		ISpeciesRoot speciesRoot = chromosomeType.getSpeciesRoot();
 		IChromosomeType[] chromosomeTypes = speciesRoot.getKaryotype();
-		int types = readVarInt();
-
 		ChromosomeInfo info = new ChromosomeInfo(chromosomeType);
 
 		for (IChromosomeType type : chromosomeTypes) {
-			int index = type.ordinal();
-			int bit = (1 << chromosomeTypes.length - index - 1);
-			boolean isValid = (bit & types) == bit;
 			if(type == chromosomeType){
-				if(!isValid){
-					return info;
-				}
-				return info.setChromosome(readChromosome(type, true, info));
+				return info.setChromosome(readChromosome(type, info));
 			} else if(type == speciesRoot.getSpeciesChromosomeType()){
-				Chromosome chromosome = readChromosome(type, isValid, info);
+				Chromosome chromosome = readChromosome(type, info);
 
 				info.setSpeciesInfo(chromosome.getActiveAllele().getUID(), chromosome.getInactiveAllele().getUID());
-			}else if(isValid){
+			}else{
 				readVarInt();
 				readVarInt();
 			}
