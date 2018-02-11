65325ae7d5aaefc3e21fa21e8af9b7456f27af32
temp1011 <34900092+temp1011@users.noreply.github.com>
2018-02-11 12:11:46 +0000

    Add IMC to let mods add fertilizer

--- src/main/java/forestry/farming/ModuleFarming.java
+++ src/main/java/forestry/farming/ModuleFarming.java
@@ -33,6 +33,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.oredict.OreDictionary;
 
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInterModComms.IMCMessage;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -46,6 +47,8 @@ import forestry.api.core.ForestryAPI;
 import forestry.api.farming.IFarmProperties;
 import forestry.api.farming.IFarmRegistry;
 import forestry.api.modules.ForestryModule;
+import forestry.apiculture.HiveConfig;
+import forestry.apiculture.blocks.BlockCandle;
 import forestry.core.ModuleCore;
 import forestry.core.blocks.BlockBogEarth;
 import forestry.core.blocks.BlockRegistryCore;
@@ -57,6 +60,8 @@ import forestry.core.config.LocalizedConfiguration;
 import forestry.core.items.EnumElectronTube;
 import forestry.core.items.ItemRegistryCore;
 import forestry.core.recipes.RecipeUtil;
+import forestry.core.utils.IMCUtil;
+import forestry.core.utils.Log;
 import forestry.core.utils.OreDictUtil;
 import forestry.farming.blocks.BlockMushroom;
 import forestry.farming.blocks.BlockRegistryFarming;
@@ -354,5 +359,20 @@ public class ModuleFarming extends BlankForestryModule {
 	@SideOnly(Side.CLIENT)
 	public void handleTextureRemap(TextureStitchEvent.Pre event) {
 		EnumFarmBlockType.registerSprites();
+	}
+	
+	@Override
+	public boolean processIMCMessage(IMCMessage message) {
+		if (message.key.equals("add-fertilizer")) {
+			ItemStack value = message.getItemStackValue();
+			int fertilizerValue = Integer.parseInt(message.getStringValue());
+			if (value != null) {
+				ForestryAPI.farmRegistry.registerFertilizer(value, fertilizerValue);
+			} else {
+				IMCUtil.logInvalidIMCMessage(message);
+			}
+			return true;
+		}  
+		return false;
 	}
 }
