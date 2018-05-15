4c043014bc4eff1d349443f3043e18ab6199e312
temp1011 <temp1011github@gmail.com>
2018-05-15 02:18:05 +0100

    add license headers where applicable

--- src/main/java/forestry/core/recipes/json/RecipeMeta.java
+++ src/main/java/forestry/core/recipes/json/RecipeMeta.java
@@ -1,3 +1,15 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2011-2014 SirSengir.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Various Contributors including, but not limited to:
+ * SirSengir (original work), CovertJaguar, Player, Binnie, MysteriousAges
+ *******************************************************************************
+ */
 package forestry.core.recipes.json;
 
 import javax.annotation.Nullable;
@@ -10,7 +22,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 public class RecipeMeta implements IRecipe {
-
+	//TODO - remove if unneeded? maybe replace with property based recipe
 	private Item inputItem;
 	private Item outputItem;
 	private int meta = 0;
