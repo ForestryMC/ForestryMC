439d7255493a7c2a2c2c3c0375fa6c62e83df6ef
therealfarfetchd <git@marco-rebhan.de>
2018-06-08 20:13:35 +0200

    fix energy accept

--- src/main/java/forestry/energy/compat/mj/MjReceiverWrapper.java
+++ src/main/java/forestry/energy/compat/mj/MjReceiverWrapper.java
@@ -6,6 +6,8 @@ import forestry.core.config.Constants;
 import forestry.energy.EnergyManager;
 import net.minecraftforge.fml.common.Optional;
 
+import static java.lang.Math.min;
+
 @Optional.Interface(iface = "buildcraft.api.mj.IMjReceiver", modid = Constants.BCLIB_MOD_ID)
 public class MjReceiverWrapper extends MjConnectorWrapper implements IMjReceiver, IMjRedstoneReceiver {
 	public MjReceiverWrapper(EnergyManager energyManager) {
@@ -14,11 +16,11 @@ public class MjReceiverWrapper extends MjConnectorWrapper implements IMjReceiver
 
 	@Override
 	public long getPowerRequested() {
-		return MjHelper.toMicroJoules(energyManager.getMaxEnergyReceived());
+		return MjHelper.toMicroJoules(min(energyManager.getMaxEnergyReceived(), energyManager.getMaxEnergyStored() - energyManager.getEnergyStored()));
 	}
 
 	@Override
 	public long receivePower(long microJoules, boolean simulate) {
-		return MjHelper.toMicroJoules(energyManager.receiveEnergy(MjHelper.fromMicroJoules(microJoules), simulate));
+		return microJoules - MjHelper.toMicroJoules(energyManager.receiveEnergy(MjHelper.fromMicroJoules(microJoules), simulate));
 	}
 }
