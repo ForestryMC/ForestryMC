31213d3923163464c4cb11f15853d0641d08ff75
Nedelosk <nedelosk@gmail.com>
2018-09-28 18:54:36 +0200

    Fix #2255 Menril Seeds from Mystical Agriculture

--- src/main/java/forestry/plugins/PluginMysticalAgriculture.java
+++ src/main/java/forestry/plugins/PluginMysticalAgriculture.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableList;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IStringSerializable;
 
 import net.minecraftforge.fluids.FluidStack;
 
@@ -14,6 +15,7 @@ import forestry.api.modules.ForestryModule;
 import forestry.api.recipes.RecipeManagers;
 import forestry.core.config.Constants;
 import forestry.core.fluids.Fluids;
+import forestry.core.utils.Log;
 import forestry.farming.logic.ForestryFarmIdentifier;
 import forestry.farming.logic.farmables.FarmableAgingCrop;
 import forestry.modules.ForestryModuleUids;
@@ -31,134 +33,14 @@ public class PluginMysticalAgriculture extends CompatPlugin {
 	@Override
 	public void registerRecipes() {
 		if (ModuleHelper.isEnabled(ForestryModuleUids.FARMING)) {
-			ImmutableList<String> crops = ImmutableList.of(
-				"stone",
-				"dirt",
-				"nature",
-				"wood",
-				"water",
-				"ice",
-				"fire",
-				"dye",
-				"nether",
-				"coal",
-				"iron",
-				"nether_quartz",
-				"glowstone",
-				"redstone",
-				"obsidian",
-				"gold",
-				"lapis_lazuli",
-				"end",
-				"experience",
-				"diamond",
-				"emerald",
-				"zombie",
-				"pig",
-				"chicken",
-				"cow",
-				"sheep",
-				"slime",
-				"skeleton",
-				"creeper",
-				"spider",
-				"rabbit",
-				"guardian",
-				"blaze",
-				"ghast",
-				"enderman",
-				"wither_skeleton",
-				"rubber",
-				"silicon",
-				"sulfur",
-				"aluminum",
-				"copper",
-				"saltpeter",
-				"tin",
-				"bronze",
-				"zinc",
-				"brass",
-				"silver",
-				"lead",
-				"steel",
-				"nickel",
-				"constantan",
-				"electrum",
-				"invar",
-				"mithril",
-				"tungsten",
-				"titanium",
-				"chrome",
-				"platinum",
-				"iridium",
-				"ruby",
-				"sapphire",
-				"peridot",
-				"amber",
-				"topaz",
-				"malachite",
-				"tanzanite",
-				"blizz",
-				"blitz",
-				"basalz",
-				"signalum",
-				"lumium",
-				"enderium",
-				"aluminum_brass",
-				"knightslime",
-				"ardite",
-				"cobalt",
-				"manyullyn",
-				"electrical_steel",
-				"redstone_alloy",
-				"conductive_iron",
-				"soularium", "dark_steel",
-				"pulsating_iron",
-				"energetic_alloy",
-				"vibrant_alloy",
-				"mystical_flower",
-				"manasteel",
-				"terrasteel",
-				"osmium",
-				"glowstone_ingot",
-				"refined_obsidian",
-				"aquarium",
-				"cold_iron",
-				"star_steel",
-				"adamantine",
-				"marble",
-				"limestone",
-				"basalt",
-				"apatite",
-				"steeleaf",
-				"ironwood",
-				"knightmetal",
-				"fiery_ingot",
-				"meteoric_iron",
-				"desh",
-				"black_quartz",
-				"vinteum",
-				"chimerite",
-				"blue_topaz",
-				"moonstone",
-				"sunstone",
-				"aquamarine",
-				"starmetal",
-				"rock_crystal",
-				"ender_biotite",
-				"slate",
-				"ender_amethyst",
-				"draconium",
-				"yellorium",
-				"certus_quartz",
-				"fluix",
-				"quartz_enriched_iron"
-			);
-
+			ImmutableList<String> cropNames = getCropNames();
+			if(cropNames.isEmpty()){
+				return;
+			}
 			IFarmRegistry farmRegistry = ForestryAPI.farmRegistry;
 			int seedAmount = ForestryAPI.activeMode.getIntegerSetting("squeezer.liquid.seed");
 			FluidStack seedOil = Fluids.SEED_OIL.getFluid(seedAmount);
-			for (String cropName : crops) {
+			for (String cropName : cropNames) {
 				ItemStack seeds = getItemStack(cropName + "_seeds");
 				Block block = getBlock(cropName + "_crop");
 				if (seeds != null && seedOil != null) {
@@ -178,6 +60,25 @@ public class PluginMysticalAgriculture extends CompatPlugin {
 					farmRegistry.registerFarmables(ForestryFarmIdentifier.CROPS, new FarmableAgingCrop(seeds, block, BlockCrops.AGE, 7, 0));
 				}
 			}
+		}
+	}
+
+	private ImmutableList<String> getCropNames(){
+		try {
+			Class<?> typeEnum = Class.forName("com.blakebr0.mysticalagriculture.lib.CropType$Type");
+			if(typeEnum.getEnumConstants() == null){
+				return ImmutableList.of();
+			}
+			ImmutableList.Builder<String> builder = new ImmutableList.Builder<>();
+			for(Object obj : typeEnum.getEnumConstants()){
+				if(obj instanceof IStringSerializable){
+					builder.add(((IStringSerializable) obj).getName());
+				}
+			}
+			return builder.build();
+		}catch (ClassNotFoundException e){
+			Log.error("Failed to find the class ('com.blakebr0.mysticalagriculture.lib.CropType$Type') that contains the crop types of 'Mystical Agriculture'.");
+			return ImmutableList.of();
 		}
 	}
 }
