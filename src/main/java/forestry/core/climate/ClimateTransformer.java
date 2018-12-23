2f549f0b17b7788562bf74d87c1989903db99fee
temp1011 <temp1011github@gmail.com>
2018-12-23 13:24:33 +0000

    fix bug in climate manipulator builder

--- src/main/java/forestry/core/climate/ClimateTransformer.java
+++ src/main/java/forestry/core/climate/ClimateTransformer.java
@@ -117,7 +117,7 @@ public class ClimateTransformer implements IClimateTransformer, IStreamable, INb
 	@Override
 	public IClimateManipulatorBuilder createManipulator(ClimateType type) {
 		return new ClimateManipulator.Builder()
-					.setAllowBackwards()
+				.setAllowBackwards()
 			.setDefault(defaultState)
 			.setCurrent(currentState)
 			.setTarget(targetedState)
