00e7d898674f6a669875220bc833442fb7a2572a
temp1011 <34900092+temp1011@users.noreply.github.com>
2019-07-31 15:34:26 +0100

    fix potential bug for setting multiblock active (#2430)

--- src/main/java/forestry/core/network/packets/PacketActiveUpdate.java
+++ src/main/java/forestry/core/network/packets/PacketActiveUpdate.java
@@ -59,7 +59,8 @@ public class PacketActiveUpdate extends ForestryPacket implements IForestryPacke
 			TileEntity tile = TileUtil.getTile(world, pos);
 			if (tile instanceof IActivatable) {
 				((IActivatable) tile).setActive(active);
-			} else if (tile instanceof IMultiblockComponent) {
+			}
+			if (tile instanceof IMultiblockComponent) {
 				IMultiblockComponent component = (IMultiblockComponent) tile;
 				if (component.getMultiblockLogic().isConnected() && component.getMultiblockLogic().getController() instanceof IActivatable) {
 					((IActivatable) component.getMultiblockLogic().getController()).setActive(active);
