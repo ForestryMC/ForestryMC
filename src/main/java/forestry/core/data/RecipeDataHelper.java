5478ff66ca924b69988ddc401d2bdd34b8dbd7fd
temp1011 <temp1011github@gmail.com>
2019-09-11 00:12:46 +0100

    add license headers

--- src/main/java/forestry/core/data/RecipeDataHelper.java
+++ src/main/java/forestry/core/data/RecipeDataHelper.java
@@ -1,7 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2014 SirSengir.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Various Contributors including, but not limited to:
+ * SirSengir (original work), CovertJaguar, Player, Binnie, MysteriousAges
+ ******************************************************************************/
 package forestry.core.data;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
 import net.minecraft.data.IFinishedRecipe;
