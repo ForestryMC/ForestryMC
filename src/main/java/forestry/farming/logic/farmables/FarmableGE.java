68c52f899f5872d1e56777109afddcd5a130826f
temp1011 <34900092+temp1011@users.noreply.github.com>
2019-09-18 10:40:43 +0100

    [1.12] allow farms to pick up fruit/nuts dropped from decaying leaves (#2463)
    * Allow farms to pick up fruit/nuts dropped from decaying leaves
    * Fix typo
    * Use set instead of map

--- src/main/java/forestry/farming/logic/farmables/FarmableGE.java
+++ src/main/java/forestry/farming/logic/farmables/FarmableGE.java
@@ -10,6 +10,14 @@
  ******************************************************************************/
 package forestry.farming.logic.farmables;
 
+import com.google.common.collect.Sets;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
@@ -22,10 +30,26 @@ import forestry.api.arboriculture.ITreeRoot;
 import forestry.api.arboriculture.TreeManager;
 import forestry.api.farming.ICrop;
 import forestry.api.farming.IFarmable;
+import forestry.api.genetics.AlleleManager;
 import forestry.arboriculture.ModuleArboriculture;
+import forestry.core.utils.datastructures.ItemStackMap;
 import forestry.farming.logic.crops.CropDestroy;
 
 public class FarmableGE implements IFarmable {
+
+	//StackMap used because a normal HashSet didn't seem to work
+	//TODO use items instead and normal set if this is still used in 1.14+ since items are flattened
+	private final Set<ItemStack> windfall = Collections.newSetFromMap(new ItemStackMap<>());
+
+	//TODO would be nice to make this class more granular so windfall and germling checks could be more specific
+	public FarmableGE() {
+		windfall.addAll(AlleleManager.alleleRegistry.getRegisteredFruitFamilies().values().stream()
+				.map(TreeManager.treeRoot::getFruitProvidersForFruitFamily)
+				.flatMap(Collection::stream)
+				.map(p -> Sets.union(p.getProducts().keySet(), p.getSpecialty().keySet()))
+				.flatMap(Collection::stream)
+				.collect(Collectors.toSet()));
+	}
 
 	@Override
 	public boolean isSaplingAt(World world, BlockPos pos, IBlockState blockState) {
@@ -33,6 +57,7 @@ public class FarmableGE implements IFarmable {
 	}
 
 	@Override
+	@Nullable
 	public ICrop getCropAt(World world, BlockPos pos, IBlockState blockState) {
 		Block block = blockState.getBlock();
 
@@ -58,7 +83,7 @@ public class FarmableGE implements IFarmable {
 
 	@Override
 	public boolean isWindfall(ItemStack itemstack) {
-		return false;
+		return windfall.contains(itemstack);
 	}
 
 }
