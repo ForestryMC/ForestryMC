92c3a914203afc9cf6ce151caf1c0096d25a47fc
temp1011 <temp1011github@gmail.com>
2019-09-21 23:48:34 +0100

    fix farm models

--- src/main/java/forestry/core/data/ForestryItemModelProvider.java
+++ src/main/java/forestry/core/data/ForestryItemModelProvider.java
@@ -1,9 +1,16 @@
 package forestry.core.data;
 
+import com.google.common.collect.Table;
+
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.BlockItem;
 
+import forestry.cultivation.blocks.BlockPlanter;
+import forestry.cultivation.blocks.BlockTypePlanter;
+import forestry.cultivation.features.CultivationBlocks;
 import forestry.farming.features.FarmingBlocks;
+import forestry.modules.features.FeatureBlock;
 
 public class ForestryItemModelProvider extends ModelProvider {
 	public ForestryItemModelProvider(DataGenerator generator) {
@@ -14,6 +21,11 @@ public class ForestryItemModelProvider extends ModelProvider {
 	protected void registerModels() {
 		for (BlockItem farm : FarmingBlocks.FARM.getItems()) {
 			registerModel(farm, new ModelBuilder().parent("forestry:block/farm"));
+		}
+
+		for(Table.Cell<BlockTypePlanter, BlockPlanter.Mode, FeatureBlock<BlockPlanter, BlockItem>> cell : CultivationBlocks.PLANTER.getFeatureByTypes().cellSet()) {
+			Block block = cell.getValue().block();
+			registerModel(block, new ModelBuilder().parent("forestry:block/" + cell.getRowKey().getName()));
 		}
 	}
 }
