55255bd4d6e6e8eabbed66d607a064458a260f32
Theodore Stites <argionad@gmail.com>
2020-05-27 23:55:20 -0400

    Add config option to modify beehive light level - issue #2237

--- src/main/java/forestry/core/config/Config.java
+++ src/main/java/forestry/core/config/Config.java
@@ -61,6 +61,7 @@ public class Config {
 
 	// Graphics
 	public static boolean enableParticleFX = true;
+	public static float hiveLightLevel = 0.4F;
 
 	// Humus
 	public static int humusDegradeDelimiter = 3;
@@ -298,6 +299,7 @@ public class Config {
 			enableEnergyStat = configCommon.getBooleanLocalized("tweaks.gui.tabs", "energy", enableEnergyStat);
 
 			enableParticleFX = configCommon.getBooleanLocalized("performance", "particleFX", enableParticleFX);
+			hiveLightLevel = configCommon.getFloatLocalized("performance", "hiveLightLevel", hiveLightLevel, 0.0f, 0.4f);
 		}
 
 		farmSize = configCommon.getIntLocalized("tweaks.farms", "size", farmSize, 1, 3);
