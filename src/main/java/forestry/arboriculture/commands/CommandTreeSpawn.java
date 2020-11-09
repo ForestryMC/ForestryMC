89a960152a78ef9275ebb2446bdc04e0121bb734
Waterpicker <waterpickerenternity@gmail.com>
2020-11-09 17:01:23 -0600

    Updated Tree Spawn based on suggestion.

--- src/main/java/forestry/arboriculture/commands/CommandTreeSpawn.java
+++ src/main/java/forestry/arboriculture/commands/CommandTreeSpawn.java
@@ -19,13 +19,11 @@ import net.minecraft.command.Commands;
 import net.minecraft.command.ISuggestionProvider;
 import net.minecraft.util.ResourceLocation;
 
-import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
@@ -33,10 +31,10 @@ import forestry.api.arboriculture.TreeManager;
 import forestry.api.arboriculture.genetics.ITree;
 import forestry.api.arboriculture.genetics.TreeChromosomes;
 import forestry.arboriculture.genetics.TreeDefinition;
-import genetics.commands.PermLevel;
 
 import genetics.api.alleles.IAllele;
 import genetics.api.individual.IIndividual;
+import genetics.commands.PermLevel;
 
 public final class CommandTreeSpawn {
 	public static ArgumentBuilder<CommandSource, ?> register(String name, ITreeSpawner treeSpawner) {
@@ -59,8 +57,7 @@ public final class CommandTreeSpawn {
 		@Override
 		public ITree parse(final StringReader reader) throws CommandSyntaxException {
 			ResourceLocation location = ResourceLocation.read(reader);
-
-			return TreeManager.treeRoot.getIndividualTemplates().stream().filter(a -> a.getGenome().getActiveAllele(TreeChromosomes.SPECIES).getRegistryName().equals(location)).findFirst().orElseThrow(() -> new SimpleCommandExceptionType(new LiteralMessage("Invalid Bee Type: " + location)).create());
+			return TreeManager.treeRoot.templateAsIndividual(TreeManager.treeRoot.getTemplate(location.toString()));
 		}
 
 		@Override
