e94f967c3843f7e3d99d2853252254825d20977e
Jakub <53441451+kuba6000@users.noreply.github.com>
2022-10-13 16:05:44 +0200

    Move IAlleleBeeAcceleratableEffect from GT (#34)

---
+++ src/main/java/forestry/api/apiculture/IAlleleBeeAcceleratableEffect.java
@@ -0,0 +1,8 @@
+package forestry.api.apiculture;
+
+import forestry.api.genetics.IEffectData;
+
+@SuppressWarnings("unused")
+public interface IAlleleBeeAcceleratableEffect {
+    IEffectData doEffectAccelerated(IBeeGenome genome, IEffectData storedData, IBeeHousing housing, float did);
+}
