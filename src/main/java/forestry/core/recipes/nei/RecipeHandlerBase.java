5ef81da4aa11a77e150d18d7cf449065071e2e7d
Alastors <78517796+Alastors@users.noreply.github.com>
2023-04-26 20:40:04 -0400

    Fixes the incompatability with the newest NEI
    Thanks Mitchbama
    
    Co-Authored-By: Jason Mitchell <1894689+mitchej123@users.noreply.github.com>

--- src/main/java/forestry/core/recipes/nei/RecipeHandlerBase.java
+++ src/main/java/forestry/core/recipes/nei/RecipeHandlerBase.java
@@ -109,7 +109,7 @@ public abstract class RecipeHandlerBase extends TemplateRecipeHandler implements
     public void loadUsageRecipes(FluidStack ingredient) {}
 
     @Override
-    public List<String> handleTooltip(GuiRecipe guiRecipe, List<String> currenttip, int recipe) {
+    public List<String> handleTooltip(GuiRecipe<?> guiRecipe, List<String> currenttip, int recipe) {
         super.handleTooltip(guiRecipe, currenttip, recipe);
         CachedBaseRecipe crecipe = (CachedBaseRecipe) this.arecipes.get(recipe);
         if (GuiContainerManager.shouldShowTooltip(guiRecipe)) {
@@ -125,7 +125,7 @@ public abstract class RecipeHandlerBase extends TemplateRecipeHandler implements
     }
 
     @Override
-    public List<String> handleItemTooltip(GuiRecipe guiRecipe, ItemStack itemStack, List<String> currenttip,
+    public List<String> handleItemTooltip(GuiRecipe<?> guiRecipe, ItemStack itemStack, List<String> currenttip,
             int recipe) {
         super.handleItemTooltip(guiRecipe, itemStack, currenttip, recipe);
         CachedBaseRecipe crecipe = (CachedBaseRecipe) this.arecipes.get(recipe);
